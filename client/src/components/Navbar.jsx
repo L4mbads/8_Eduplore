9ad9202208207489462e3d6cc938829dbe01b7d4
Azzkaaaa <19623121@std.stei.itb.ac.id>
2024-08-25 20:28:42 +0700

    fix: Typo beasiswaRef.current? (from layananRef.current? to beasiswaRef.current? on line 26) and Navlink to button on line 16, 23, 24, and 31.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -13,22 +13,22 @@ export default function Navbar() {
                     <img alt="Eduplore" className="h-10 inline" src="../src/assets/eduplore(nama).png"></img>
                 </NavLink>
                 <div className="">
-                    <NavLink className="inline-flex items-center justify-center whitespace-nowrap text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 
+                    <button className="inline-flex items-center justify-center whitespace-nowrap text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 
                     disabled:pointer-events-none disabled:opacity-50 h-9 rounded-md px-3" to="/create"  onClick={() => {
                         layananRef.current?.scrollIntoView({
                             behavior: "smooth"
                         })
                     }}>
                         Program Kami
-                    </NavLink>
-                    <NavLink className="inline-flex items-center justify-center whitespace-nowrap text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 
+                    </button>
+                    <button className="inline-flex items-center justify-center whitespace-nowrap text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 
                     disabled:pointer-events-none disabled:opacity-50 h-9 rounded-md px-3" to="/create" onClick={() => {
-                        layananRef.current?.scrollIntoView({
+                        beasiswaRef.current?.scrollIntoView({
                             behavior: "smooth"
                         })
                     }}>
                         Beasiswa
-                    </NavLink>
+                    </button>
                     <NavLink className="inline-flex items-center justify-center whitespace-nowrap text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 
                     disabled:pointer-events-none disabled:opacity-50 h-9 rounded-md px-3 " to="/create">
                         Tentang Kami
