9e3d09760ef0475fa5200703047ba0eedf0565c5
L4mbads <rizacal.mamen@gmail.com>
2024-08-27 12:09:31 +0700

    feat: supercamp buttons

--- client/src/components/SuperCamp.jsx
+++ client/src/components/SuperCamp.jsx
@@ -1,11 +1,30 @@
 import { useState, useEffect } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
+const options = [
+    [
+        { label: 'PILIH BEASISWA', value: "" },
+        { label: 'Indonesian International Student Mobility Awards (IISMA)', value: "Indonesian International Student Mobility Awards (IISMA)" },
+        { label: 'Beasiswa Unggulan Kemendikbudristek 2024', value: "Beasiswa Unggulan Kemendikbudristek 2024" },
+    ],
+    [
+        { label: 'PILIH BATCH', value: 0 },
+        { label: 'BATCH 1', value: 1 },
+        { label: 'BATCH 2', value: 2 },
+    ],
+]
 
-
-export default function SuperExclusive() {
+export default function SuperCamp() {
+    const [packet, setPacket] = useState({
+        name: "",
+        email: "",
+        mentorId: "",
+        mentor: "",
+        beasiswa: "",
+        batch: "",
+        index: 1
+    });
     const [mentorList, setMentorList] = useState([]);
-    const [mentorIndex, setMentorIndex] = useState(-1);
     const navigate = useNavigate();
     useEffect(() => {
         async function getMentorList() {
@@ -22,23 +41,76 @@ export default function SuperExclusive() {
         return;
     }, [navigate]);
 
-    const MentorCard = (props) => {
-        return (
-            <div className={`transition duration-300 flex flex-col focus:bg-black justify-start items-center shadow-lg rounded-lg p-4 flex-none min-w-60 hover:scale-110
-        ${mentorIndex == props.id ? 'bg-green-60 scale-110' : 'bg-gray'}`}
-                onClick={() => setMentorIndex(props.id)}
-            >
-                <div className="self-start justify-center items-center flex flex-row mb-4">
-                    <img src="../src/assets/eye.svg" alt="viewer" className="size-4 mr-1" />
-                    <p className="text-blue font-semibold text-sm">1.2K</p>
-                </div>
-                <img src="../src/assets/avatar.svg" alt="avatar" className="w-1/2 mb-6" />
-                <h3 className="font-semibold text-xl">{props.data.name}</h3>
-                <p className="text-blue font-medium mb-6 text-sm">{props.data.award}</p>
-
-            </div>
-        )
+    function a() {
+        navigate('/');
     }
+    // These methods will update the state properties.
+    function updatePacket(value) {
+        return setPacket((prev) => {
+            return { ...prev, ...value };
+        });
+    }
+
+    async function onDaftar(e) {
+        e.preventDefault();
+
+
+
+        const authorized = await fetch(`http://localhost:5050/auth/`, {
+            method: "GET",
+            credentials: "include",
+        });
+        if (!authorized.ok) {
+            navigate("/login");
+            return;
+        }
+
+
+        const user = await authorized.json();
+
+        const response = await fetch(`http://localhost:5050/user-management/user/${user.id.toString()}`, {
+            method: "GET",
+            credentials: "include",
+        });
+        if (!response.ok) {
+            return;
+        }
+
+        const userData = await response.json();
+        packet.name = await userData.name;
+        packet.email = await userData.email;
+        try {
+
+            const person = { ...packet }
+            let response;
+            // if we are adding a new record we will POST to /record.
+            response = await fetch("http://localhost:5050/email-sender/", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(person),
+            });
+
+
+        } catch (error) {
+            console.error('A problem occurred with your fetch operation: ', error);
+        }
+
+
+    }
+    const Select = ({ value, options, onChange }) => {
+        return (
+            <select value={value} onChange={onChange} className="mb-2 pr-3 w-full text-2xl font-bold focus:ring-0 focus:ring-offset-0">
+                {options.map((option) => (
+                    <option value={option.value} className={`text-2xl font-bold ${option.value != '' ? 'text-blue' : ''}`}>{option.label}</option>
+                ))}
+            </select>
+        );
+    };
+    const handleRadioChange = (e) => {
+        updatePacket({ beasiswa: e.target.value });
+    };
 
     return (
         <div className="relative flex flex-col w-full items-start bg-gradient-to-b from-blue to-transparent from-30% to-65% min-h-screen pb-16">
@@ -49,7 +121,7 @@ export default function SuperExclusive() {
             </div>
             <div className="flex flex-col w-full justify-center md:justify-start items-center md:items-start px-40 mb-16 ">
                 <h1 className=" text-white font-bold tracking-wide text-5xl ">
-                    SUPER-EXCLUSIVE
+                    SUPERCAMP
                 </h1>
                 <h3 className="bg-green text-black font-semibold mt-4 px-2 py-1 rounded-xl">
                     Rating: 4.7
@@ -62,34 +134,27 @@ export default function SuperExclusive() {
                     </h2>
                     <p className="text-xl">
                         Kelas intensif bersama para awardee selama rangkaian persiapan beasiswa.
-                        Membahas persiapan yang diperlukan serta tips dan trik mulai dari seleksi berkas hingga wancara.`
+                        Membahas persiapan yang diperlukan serta tips dan trik mulai dari seleksi berkas hingga wancara.
                     </p>
                 </div>
                 <div className="grow bg-white rounded-xl p-10 shadow-lg">
-                    <h2 className="text-2xl font-bold pb-4">
-                        PILIH MENTOR
-                    </h2>
-                    <div className="flex flex-row">
-                        <div className="flex flex-nowrap gap-x-10 p-6 overflow-x-auto">
-
-                            {mentorList.map((mentor, index) => {
-                                return <MentorCard
-                                    data={mentor}
-                                    id={index}
-                                /> //to implement page, use slice((PAGE-1) * amount, PAGE * amount)
-                            })}
-
-                        </div>
-                        <img src="../src/assets/arrow.svg" alt="" className="size-16 ml-8 grow self-center rotate-180" />
-                    </div>
+                    <Select
+                        options={options[0]}
+                        value={packet.beasiswa}
+                        onChange={(e) => updatePacket({ beasiswa: e.target.value })}
+                    />
                 </div>
                 <div className="grow bg-white rounded-xl p-10 shadow-lg">
-                    <h2 className="text-2xl font-bold">
-                        PILIH BATCH
-                    </h2>
 
+                    <Select
+                        options={options[1]}
+                        value={packet.batch}
+                        onChange={(e) => updatePacket({ batch: e.target.value })}
+                    />
                 </div>
-                <button className="transition grow bg-orange rounded-xl p-4 shadow-lg text-white font-bold text-3xl tracking-widest hover:bg-orange-80 hover:scale-110">
+                <button className="transition grow bg-orange rounded-xl p-4 shadow-lg text-white font-bold text-3xl tracking-widest hover:bg-orange-80 hover:scale-110"
+                    onClick={onDaftar}
+                >
                     DAFTAR
                 </button>
             </div>
