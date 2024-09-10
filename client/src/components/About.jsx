1cb9247c7ad85c86c65fd30a6eb72444cdb7c44d
I Made Wiweka Putera <78787384+wiwekaputera@users.noreply.github.com>
2024-09-10 14:57:45 +0700

    fix text duplicates

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -15,28 +15,6 @@ export default function About() {
           </p>
         </div>
       </div>
-      <div className="team-section mt-5 p-5 bg-white bg-opacity-80 rounded-lg ml-20 mr-20">
-        <h2 className="text-3xl font-bold mb-5">Our Team</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          <div className="left-column sm:flex-wrap">
-            <div className="flex mb-4">
-              <h3 className="w-48 role text-lg font-semibold mr-4 md:whitespace-nowrap sm:flex-wrap pr-8">Product Manager</h3>
-              <div className="flex-1">
-                <p className="name font-semibold">M Zidni Alkindi</p>
-                <p className="name font-semibold">Nicholas Zefanya</p>
-              </div>
-            </div>
-            <div className="flex mb-4">
-              <h3 className="w-48 role text-lg font-semibold mr-4 whitespace-nowrap pr-8">Software Developer</h3>
-              <div className="flex-1">
-                <p className="name font-semibold">Ghazy Achmed</p>
-                <p className="name font-semibold">M Aulia Azka</p>
-                <p className="name font-semibold">Fachriza Ahmad</p>
-                <p className="name font-semibold">I Made Wiweka</p>
-              </div>
-            </div>
-          </div>
-        </div>
         <div className="team-section mt-5 p-5 bg-white bg-opacity-80 rounded-lg ml-20 mr-20">
           <h2 className="text-3xl font-bold mb-5">Our Team</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
@@ -82,7 +60,6 @@ export default function About() {
             </div>
           </div>
         </div>
-      </div>
       <Footer />
     </div>
   );
